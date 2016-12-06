152a0df00c41a4f82de2ec4cfd0ccee9d9ce8e18
zaslavskayasya <web.zaslavskaya@gmail.com>
2016-12-06 14:17:11 +0200

    then...

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.controller('weathersController', function ($scope, API) {
         this.tab = tab;
     };
 
+    API.getWeatherCurrent().then(function (my_current) {
+        $scope.weatherCurrent = my_current;
+    });
+
     $scope.weatherCurrent = []
     ;
     $scope.weatherForecast = [
@@ -27,11 +31,23 @@ app.service('API', function ($http) {
                 method: 'GET',
                 url: 'http://api.apixu.com/v1/current.json',
                 params: {
-                    key : '5c40c72b9b544023b9b74029162111',
-                    q: 'Odessa'
+                    key : key,
+                    q: 'Odessa Ukraine'
                 }
-            })
-
+            }).then(function (data) {
+                var acurrent = [data.data.current];
+               /* console.log(api_current);*/
+                var my_current = acurrent.map(function (api_current, index){
+                    return {
+                        dateUpdate: api_current.last_updated,
+                        temp: api_current.temp_c,
+                        wind: api_current.wind_kph,
+                        humidity: api_current.humidity,
+                        feelslike: api_current.feelslike_c
+                    }
+                });
+                console.log(my_current);
+            });
         }
     }
 });
