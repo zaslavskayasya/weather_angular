98199a6f2ab7ee5dc5334ab507fc73ceb1fff6f1
zaslavskayasya <web.zaslavskaya@gmail.com>
2016-12-12 22:44:44 +0200

    +geo +design

--- app.js
+++ app.js
@@ -1,63 +1,65 @@
 var app = angular.module('weather', []);
 
-/*app.controller('weathersController', function ($scope) {
-    $scope.weatherCurrent = [ ];
-    $scope.weatherForecast = [ ];
-}
-*/
 var lat;
-var Pos;
-var coord1;
-var coord2;
+var lon;
 var city = "Kiev Ukraine";
-
 
 app.controller('weathersController', function ($scope, API) {
     this.tab = 1 ;
     this.selectTab = function (tab) {
         this.tab = tab;
     };
-    /*navigator.geolocation.getCurrentPosition(function(position) {
-         lat = position.coords.latitude;
-         Pos = position.coords.longitude;
-             lat = lat.toFixed(1);
-             Pos = Pos.toFixed(1);
-          console.log(typeof lat);
-          od = lat +"," + Pos;
-        console.log(od);
-        console.log(typeof od);
-    })
-    coord1 = lat;
-    coord2 = Pos;
-    console.log(coord1,coord2);*/
-
-    $scope.searchSettings = {
-        query: 'Kiev'
-    };
 
     $scope.searchCity = function () {
         API.SearchCity($scope.searchSettings).then(function (search) {
             $scope.search = search;
-            console.log(search);
+           /* console.log(search);*/
         });
 
         API.getWeatherCurrent($scope.searchSettings).then(function (current) {
-
             $scope.current = current;
-            console.log(current);
+            /*console.log(current);*/
         });
         API.getWeatherForecast($scope.searchSettings).then(function (forecast) {
             $scope.forecast = forecast;
             /*      console.log(forecast);*/
         });
-
     };
 
+    $scope.searchGeo = function () {
+       navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+           /* lat = lat.toFixed(1);
+            Pos = Pos.toFixed(1);*/
+            console.log(lat);
+            console.log( lon);
+            var od = lon + ',' + lat;
+            console.log(od);
+            console.log(typeof od);
+        });
+        API.SearchCity2($scope.searchSettings).then(function(od) {
+            console.log(od);
+            $scope.od = od;
+            console.log(od);
+        });
+        API.getWeatherCurrent2($scope.searchSettings).then(function (current) {
+            $scope.current = current;
+           /* console.log(current);*/
+        });
+        API.getWeatherForecast2($scope.searchSettings).then(function (forecast) {
+            $scope.forecast = forecast;
+            /*      console.log(forecast);*/
+        });
+    };
+
+    $scope.searchSettings = {
+        query: 'Kiev',
+        od: 'lat,lon'
+    };
 
     $scope.current = [];
-
     $scope.my_forecast = [ ];
-
     $scope.searched = [];
 
 });
@@ -96,7 +98,7 @@ app.service('API', function ($http, $q){
             }).then(function (data) {
                 console.log(data);
                 var forecast = data.data.forecast.forecastday;
-                console.log(forecast);
+               /* console.log(forecast);*/
 
                 d.resolve(forecast);
             });
@@ -110,6 +112,63 @@ app.service('API', function ($http, $q){
                 params: {
                     key : key,
                     q: params.query,
+                    city: name
+
+                }
+            }).then(function (data) {
+                console.log(data);
+                var search = data.data[1];
+                d.resolve(search);
+                console.log(search)
+            });
+            return d.promise;
+        },
+        getWeatherCurrent2: function (paramss) {
+            var key = '5c40c72b9b544023b9b74029162111';
+            var d = $q.defer();
+            $http({
+                method: 'GET',
+                url: 'http://api.apixu.com/v1/current.json',
+                params: {
+                    key : key,
+                    q: paramss.od
+                }
+            }).then(function (data) {
+                var current = data.data.current;
+                /* console.log(api_current);*/
+                console.log(current);
+                d.resolve(current);
+            });
+            return d.promise;
+        },
+        getWeatherForecast2: function (paramss) {
+            var key = '5c40c72b9b544023b9b74029162111';
+            var d = $q.defer();
+            $http({
+                method: 'GET',
+                url: 'http://api.apixu.com/v1/forecast.json',
+                params: {
+                    key : key,
+                    q: paramss.od,
+                    days: '7'
+                }
+            }).then(function (data) {
+                console.log(data);
+                var forecast = data.data.forecast.forecastday;
+                /* console.log(forecast);*/
+
+                d.resolve(forecast);
+            });
+            return d.promise;
+        }, SearchCity2: function (paramss) {
+            var key = '5c40c72b9b544023b9b74029162111';
+            var d = $q.defer();
+            $http({
+                method: 'GET',
+                url: 'http://api.apixu.com/v1/search.json',
+                params: {
+                    key : key,
+                    q: paramss.od,
                     city: name
 
                 }
